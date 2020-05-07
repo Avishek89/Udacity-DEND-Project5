c5d2dfba6efb7dbab270a9c370f7d79eef780178
Avihek <avishek.1989@gmail.com>
2020-05-07 13:48:13 +0800

    DEND-Udacity

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -4,25 +4,33 @@ from airflow.utils.decorators import apply_defaults
 
 
 class DataQualityOperator(BaseOperator):
-
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 aws_credentials_id="",
                  redshift_conn_id="",
-                 tables=[],
+                 table="",
+                 test_stmt=None,
+                 result=None,
                  *args, **kwargs):
+
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.aws_credentials_id = aws_credentials_id,
-        self.redshift_conn_id = redshift_conn_id,
-        self.tables = tables
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.test_stmt = test_stmt
+        self.result = result
 
     def execute(self, context):
-        redshift_hook = PostgresHook(self.redshift_conn_id)
-        for table in self.tables:
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(records) < 1 or len(records[0]) < 1 or records[0][0] < 1:
-                self.log.error(f"Data quality check failed. {table} returned no results")
-                raise ValueError(f"Data quality check failed. {table} returned no results")
-            self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+        pg_hook = PostgresHook(self.redshift_conn_id)
+        records = pg_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Fail: No results for {self.table}")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(f"Fail: 0 rows in {self.table}")
+
+        if self.test_stmt:
+            output = pg_hook.get_first(self.test_stmt)
+            if self.result != output:
+                raise ValueError(f"Fail: {output} != {self.result}")
+        self.log.info(f"Success: {self.table} has {records[0][0]} records")
